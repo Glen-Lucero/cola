cdde37c3b89cfa106b0f27c52bf92a7243c5f7f6
Glen-Lucero <glucero44@gmail.com>
2016-01-12 20:07:18 -0500

    Home Page Sections

--- index.php
+++ index.php
@@ -18,22 +18,21 @@
 		<link rel='stylesheet' href='assets/css/main.css'>
 	</head>
 	<body>
-		<div class='navbar navbar-default'>
-			<div class='container-fluid'>
+		<div class='navbar navbar-default coke-main-nav'>
+			<div class='container'>
 				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-nav" aria-expanded="false">
 						<span class="sr-only">Toggle navigation</span>
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 						<span class="icon-bar"></span>
 					</button>
-					<a class="navbar-brand" href="#">Brand</a>
+					<a class="navbar-brand" href="#">
+						<img alt="Coca-Cola" src='assets/img/coke-logo1.png'>
+					</a>
 				</div>
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav">
-						<li class="active">
-							<a href="#">Link <span class="sr-only">(current)</span></a>
-						</li>
+				<div class="collapse navbar-collapse" id="main-nav">
+					<!--<ul class="nav navbar-nav">
 						<li>
 							<a href="#">Link</a>
 						</li>
@@ -49,31 +48,36 @@
 								<li><a href="#">One more separated link</a></li>
 							</ul>
 						</li>
-					</ul>
-					<form class="navbar-form navbar-left" role="search">
-						<div class="form-group">
-							<input type="text" class="form-control" placeholder="Search">
-						</div>
-						<button type="submit" class="btn btn-default">Submit</button>
-					</form>
+					</ul>--> <!-- Save In case I come up with some use for this -->
 					<ul class="nav navbar-nav navbar-right">
 						<li>
-							<a href="#">Link</a>
+							<a href="#">Sign In</a>
 						</li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-								<ul class="dropdown-menu">
-								<li><a href="#">Action</a></li>
-								<li><a href="#">Another action</a></li>
-								<li><a href="#">Something else here</a></li>
-								<li role="separator" class="divider"></li>
-								<li><a href="#">Separated link</a></li>
-							</ul>
+						<li>
+							<a href="#">Sign Up</a>
 						</li>
 					</ul>
 				</div><!-- /.navbar-collapse -->
 			</div><!--/.container-fluid-->
 		</div><!--/.navbar-->
+		<div class='bg-main-img'></div>
+		<div class='bg-main-wrapper'></div>
+		<div class='promo'>
+			<div class="container">
+				<h1 class='text-center'>Promo Tagline</h1>
+				<p class='text-center'>
+					Ei mei platonem senserit, legendos iracundia repudiandae ad qui, sed oratio dicant ut. 
+				</p>
+			</div>	
+		</div><!-- /.promo -->
+		<div class='info-section'>
+			<div class='container'>
+			</div><!--/.container-->
+		</div><!-- /.info-section -->
+		<div class='footer'>
+			<div class='container'>
+			</div>
+		</div><!--/.footer-->
 	</body>
 	<!-- Javascript -->
 	<script type='text/Javascript' src='assets/js/jquery-1.12.0.min.js'></script>
