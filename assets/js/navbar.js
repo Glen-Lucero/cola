3eb4284b09fffa34e39fd800344ad8ceed1b227d
Glen-Lucero <glucero44@gmail.com>
2016-01-25 22:36:38 -0500

    navbar fixes

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
-	
-	$(".solution").click(function(){
-		
+	$body_content = $("#pseudo-body").html();
+
+	$(".navbar-brand").click(function(){
+		$("#pseudo-body").html($body_content);
+	});
+
+	$("li a.solution").click(function(){
+		$path = $(this).data('path');
+		$page = $(this).data('page');
+
+		if($page === "home"){
+			$("#pseudo-body").html($body_content);
+		}
+		else{
+			$("#pseudo-body").load($path + $page);
+		}
 	});
 });
--- orgs/coca-cola/assets/js/main.js
+++ orgs/coca-cola/assets/js/main.js
@@ -2,23 +2,4 @@ $(document).ready(function(){
 	$("#logout").click(function(){
 		window.location = "file:///Users/glenlucero/Lucero/index.html";
 	});
-
-	$("li a.solution").click(function(){
-		$path = $(this).data('path');
-		$page = $(this).data('page');
-
-		if($page === "home"){
-			$("#pseudo-body").html(
-				"<div class='home-bg'></div>"+
-				"<div class='home-default'>"+
-					"<div class='container'>"+
-						"<h1 class='text-center'>This is a Title</h1>"+
-						"<p class='text-center'>Ei mei platonem senserit, legendos iracundia repudiandae ad qui, sed oratio dicant ut. </p>"+
-					"</div>"+
-				"</div>");
-		}
-		else{
-			$("#pseudo-body").load($path + $page);
-		}
-	});
 });
